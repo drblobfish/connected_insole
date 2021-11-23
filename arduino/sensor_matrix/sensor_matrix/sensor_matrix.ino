5ab2386b179fade2aa4d21a87e5f259d8415b185
drblobfish <jules.herrmann@gmail.com>
2021-11-23 20:01:35 +0100

    put power on only one row at a time- successfuly tested with led

--- arduino/sensor_matrix/sensor_matrix/sensor_matrix.ino
+++ arduino/sensor_matrix/sensor_matrix/sensor_matrix.ino
@@ -2,7 +2,8 @@
  * 
  * 
  */
-
+int nb_row = 4;
+int nb_col = 6;
 
 int rowPin[] = {A0,A1,A2,A3};
 int colPin[] = {2,3,4,5,6,7};
@@ -10,10 +11,10 @@ int sensorValues[] = {0,0,0,0};
 
 void setup() {
   // put your setup code here, to run once:
-  for (int i=0;i<4;i++){
+  for (int i=0;i<nb_row;i++){
     pinMode(rowPin[i], INPUT);
     }
-  for (int i=0;i<6;i++){
+  for (int i=0;i<nb_col;i++){
     pinMode(colPin[i], OUTPUT);
     }
 
@@ -21,5 +22,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int i_col=0;i_col<nb_col;i_col++){
+    digitalWrite(colPin[i_col], HIGH);
+    for (int j_col=0;j_col<nb_col;j_col++){
+      if (i_col != j_col){
+        digitalWrite(colPin[j_col], LOW);
+        }
+      }
+    delay(100);
+    }
 
 }
