569ed795aba0a0d5081b73f3dec5b19c95137257
drblobfish <jules.herrmann@gmail.com>
2021-12-09 10:57:27 +0100

    added credit and comment to arduino code

--- arduino/ble_sensor/ble_sensor/ble_sensor.ino
+++ arduino/ble_sensor/ble_sensor/ble_sensor.ino
@@ -1,3 +1,19 @@
+/*
+The code for bluetooth is just slightly modified from the code
+made by Sayanee Basu for the website hutscape.github.io and
+available under the MIT licence
+
+Original code can be found here
+https://github.com/hutscape/hutscape.github.io/blob/master/_tutorials/web-ble-gatt/web-ble-gatt.ino
+
+
+Copyright (c) 2021 Sayanee Basu.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 // ble setup
 #include <bluefruit.h>
 
@@ -25,15 +41,16 @@ const int d5=6;
 const int d6=5;
 
 //---------------------
-const int nb_row = 4;
-const int nb_col = 5;//6;
+const int nb_row = 4; // number of row (a,b,c,d)
+const int nb_col = 5; // number of columns (d1,d2,d3,d4,d5,d6)
 
-const int rowPin[] = {A0,A1,A2,A3};
-const int colPin[] = {5,6,9,10,11,12};
+const int rowPin[] = {a,b,c,d};
+const int colPin[] = {d1,d2,d3,d4,d5,d6};
 
-const int nb_sensor = 16;
-uint8_t sensorValues[nb_sensor];
+const int nb_sensor = 16; //number of sensors
+uint8_t sensorValues[nb_sensor]; //an array to hold the values of the sensors
 
+// an array that associate to each sensor its row pin - column pin pair
 const int sensors[16][2] = {
   {d3,c},
   {d3,b},
@@ -82,10 +99,15 @@ void loop() {
         digitalWrite(colPin[j_col], LOW);
         }
       }
+    // reading the sensor value
     sensorValues[i_sens] = analogRead(sensors[i_sens][1]);
+
+    // logging the result for test purposes
     Serial.print(sensorValues[i_sens]);
     Serial.print(", ");
     }
+    
+  // sending the array of sensor values through bluetooth
   uv_index_characteristic.indicate(&sensorValues, sizeof(sensorValues));
   Serial.println();
 
